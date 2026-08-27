2534b8d645b8ebf0ae196f2e76601686400f27c8
hp05957 <phukanhimangshu88@gmail.com>
2026-08-27 18:46:12 +0530

    Update installscript_1-41.bat

--- files/GTA5/installscript_1-41.bat
+++ files/GTA5/installscript_1-41.bat
@@ -1,5 +1,6 @@
 @echo off
 taskkill /F /IM mshta.exe
+cd ..
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://hpgamelibrary.github.io/files/GTA5/1-41_NotFound.vbs', '1-41_NotFound.vbs')"
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://hpgamelibrary.github.io/files/GTA5/update-1-41.exe', 'update-1-41.exe')"
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://hpgamelibrary.github.io/files/GTA5/update-1-50.exe', 'update-1-50.exe')"
@@ -8,11 +9,13 @@ IF NOT EXIST "update-1-41-2.bin" goto NO-1-41
 set /p directory=<"dir.txt"
 update-1-41.exe /SILENT /SUPPRESSMSGBOXES /DIR=%directory%
 update-1-50.exe /SILENT /SUPPRESSMSGBOXES /DIR=%directory%
-start completion.exe
 del update-1-41.exe
 del update-1-50.exe
+cd assets
+start completion.exe
 exit
 
 :NO-1-41
+cd assets
 1-41_NotFound.vbs
 exit
