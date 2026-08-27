6009c263de9b77d5f142d125d4486f79140e9909
hp05957 <phukanhimangshu88@gmail.com>
2026-08-27 18:03:13 +0530

    Update installscript_1-41.bat

--- files/GTA5/installscript_1-41.bat
+++ files/GTA5/installscript_1-41.bat
@@ -6,3 +6,6 @@ set /p directory=<"dir.txt"
 update-1-41.exe /SILENT /SUPPRESSMSGBOXES /DIR=%directory%
 update-1-50.exe /SILENT /SUPPRESSMSGBOXES /DIR=%directory%
 start completion.exe
+del update-1-41.exe
+del update-1-50.exe
+exit
