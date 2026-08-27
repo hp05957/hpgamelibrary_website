59926ab6816222f021831f0a30398033e4c74935
hp05957 <phukanhimangshu88@gmail.com>
2026-08-27 16:05:37 +0530

    Update installscript_1-41.bat

--- files/GTA5/installscript_1-41.bat
+++ files/GTA5/installscript_1-41.bat
@@ -1,5 +1,7 @@
 @echo off
 taskkill /F /IM mshta.exe
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://hpgamelibrary.github.io/files/GTA5/update-1-41.exe', 'update-1-41.exe')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://hpgamelibrary.github.io/files/GTA5/update-1-50.exe', 'update-1-50.exe')"
 set /p directory=<"dir.txt"
 update-1-41.exe /SILENT /SUPPRESSMSGBOXES /DIR=%directory%
 update-1-50.exe /SILENT /SUPPRESSMSGBOXES /DIR=%directory%
